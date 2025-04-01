07908eb41a2aa377fa311eb51831b5812cc7618d
Maitri Chovatiya <maitrikchovatiya@gmail.com>
2025-04-01 21:50:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import GithubProfileFinder from "./components/github-profile-finder";
+
+function App() {
+  return (
+    <div className="App">
+      
+     {/* Github profile finder */}
+      <GithubProfileFinder/>
+    </div>
+  );
+}
+
+export default App;
